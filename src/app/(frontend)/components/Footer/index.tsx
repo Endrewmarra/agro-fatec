f91d3bbb4784bd713366165a9dc5897dcaf63eaa
Endrew Marra Pedrosa <dumarra11@gmail.com>
2025-11-05 17:50:00 -0300

    adjust on footer

--- src/app/(frontend)/components/Footer/index.tsx
+++ src/app/(frontend)/components/Footer/index.tsx
@@ -51,8 +51,8 @@ export const Footer = () => {
             Especialistas em consultoria agronômica para milho, soja e sorgo. Transformamos
             conhecimento em resultados práticos para o seu agronegócio.
           </Typography>
-          
-          <Box sx={{ marginLeft:-2}}>
+
+          <Box sx={{ marginLeft: -2 }}>
             <Button
               variant="outlined"
               sx={{
@@ -167,14 +167,14 @@ export const Footer = () => {
           </Typography>
         </Box>
       </Box>
-     
+
       <Box sx={{ borderTop: '2px solid #364153' }}></Box>
-      <Grid container spacing={2} sx={{color:'grey.500', marginTop:4,fontSize:14}}>
-        <Grid size={8} >© 2024 AgroConsult. Todos os direitos reservados.</Grid>
-        <Grid size={4}  sx={{display:'flex', gap:2,}}>
-          <Links rote='/' text='Termos de Uso'  />
-          <Links rote='/' text='Política de Privacidade' />
-          <Links rote='/' text='Cookies' />
+      <Grid container spacing={2} sx={{ color: 'grey.500', marginTop: 4, fontSize: 14 }}>
+        <Grid size={8}>© 2024 AgroConsult. Todos os direitos reservados.</Grid>
+        <Grid size={4} sx={{ display: 'flex', gap: 2 }}>
+          <Links rote="/" text="Termos de Uso" />
+          <Links rote="/" text="Política de Privacidade" />
+          <Links rote="/" text="Cookies" />
         </Grid>
       </Grid>
     </Box>
