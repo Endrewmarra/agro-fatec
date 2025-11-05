363968524d80d04bdca68de023c7d2f8a6fe2c6c
Endrew Marra Pedrosa <dumarra11@gmail.com>
2025-11-05 15:57:50 -0300

    Fix: styles.css import error

--- src/app/(frontend)/(app)/page.tsx
+++ src/app/(frontend)/(app)/page.tsx
@@ -1,9 +1,12 @@
+import '../styles.css'
+
 import { Box } from '@mui/material'
 import { getPayload } from 'payload'
 import config from '@payload-config'
 import { Appresentation } from '../components/Appresentation'
 import { Services } from '../components/Services'
 import { About } from '../components/About'
+
 
 
 export default async function HomePage() {
